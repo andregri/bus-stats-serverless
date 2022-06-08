16db4b0e314528df2a06c1c9fd3472b04fd94773
andregri <andrea.grillo96@live.com>
2022-06-08 23:04:57 +0200

    First commit

---
+++ lambda_post_bus.py
@@ -0,0 +1,27 @@
+import logging
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def lambda_handler(event, context):
+    stop_code = event['stop_code']
+    bus_line = event['bus_line']
+    datetime = event['datetime']
+
+    logger.info(f'{stop_code} {bus_line} {datetime}')
+
+    data = dynamodb.put_item(TableName='BusTimeTable',
+                             Item={
+                                'stop_code': {'S': stop_code},
+                                'bus_line': {'S': bus_line},
+                                'datetime': {'S': datetime}
+                             })
+
+    return {
+        'statusCode': 200,
+        'body': data
+    }
